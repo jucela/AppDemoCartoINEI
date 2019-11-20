5f3c5b3997a76124e9e318fa010b8e3f2301cfa2
sting <juliolavado5@gmail.com>
2019-11-20 02:04:27 -0500

    add google maps

--- app/src/main/java/com/inei/appcartoinei/fragments/Capa3.java
+++ app/src/main/java/com/inei/appcartoinei/fragments/Capa3.java
@@ -1,12 +1,21 @@
 package com.inei.appcartoinei.fragments;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -25,6 +34,11 @@ public class Capa3 extends Fragment implements OnMapReadyCallback {
     GoogleMap mgoogleMap;
     MapView mapView;
     View view;
+    private LocationManager mLocationManager;
+    private Location location;
+    private static final int LOCATION_REQUEST_CODE  = 1 ;
+    public static final int LOCATION_UPDATE_MIN_DISTANCE = 10;
+    public static final int LOCATION_UPDATE_MIN_TIME = 5000;
 
 
     private OnFragmentInteractionListener mListener;
@@ -36,7 +50,10 @@ public class Capa3 extends Fragment implements OnMapReadyCallback {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        mLocationManager = (LocationManager) getContext().getSystemService(Context.LOCATION_SERVICE);
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,LOCATION_UPDATE_MIN_TIME,LOCATION_UPDATE_MIN_DISTANCE, (LocationListener) this);
+        }
     }
 
     @Override
@@ -82,18 +99,68 @@ public class Capa3 extends Fragment implements OnMapReadyCallback {
         mListener = null;
     }
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        MapsInitializer.initialize(getContext());
-        mgoogleMap =googleMap;
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.689247,-74.044502)).title("prueba").snippet("xxx"));
-        CameraPosition Liberty = CameraPosition.builder().target(new LatLng(40.689247,-74.044502)).zoom(16).bearing(0).tilt(45).build();
-        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
-    }
-
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
     }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        MapsInitializer.initialize(getContext());
+        mgoogleMap =googleMap;
+        mgoogleMap.getUiSettings().setCompassEnabled(true);//Brujula
+        mgoogleMap.getUiSettings().setZoomControlsEnabled(true);//Zoom
+        mgoogleMap.getUiSettings().setMyLocationButtonEnabled(true);//GPS
+        LatLng peru = new LatLng(-9, -74);
+
+
+
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(40.689247,-74.044502)).title("prueba").snippet("xxx"));
+        CameraPosition Liberty = CameraPosition.builder().target(new LatLng(40.689247,-74.044502)).zoom(16).bearing(0).tilt(45).build();
+        googleMap.moveCamera(CameraUpdateFactory.newCameraPosition(Liberty));
+
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            mgoogleMap.setMyLocationEnabled(true);
+
+            location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (location!=null){
+                LatLng gps=new LatLng(location.getLatitude(),location.getLongitude());
+                mgoogleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(gps,12));
+            }
+            else{
+                mgoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(peru,5));
+            }
+        }
+        else {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(getActivity(),Manifest.permission.ACCESS_FINE_LOCATION)){
+            }
+
+            else{
+                ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.ACCESS_FINE_LOCATION} ,1);
+            }
+
+
+        }
+
+
+
+    }
+
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+//        if (requestCode == LOCATION_REQUEST_CODE) {
+//            // ¿Permisos asignados?
+//            if (permissions.length > 0 &&
+//                    permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) &&
+//                    grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    mgoogleMap.setMyLocationEnabled(true);
+//            } else {
+//
+//                Toast.makeText(getContext(), "Error de permisos", Toast.LENGTH_LONG).show();
+//            }
+//
+//        }
+//    }
 }
